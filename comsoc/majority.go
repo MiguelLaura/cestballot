ac5f0fd1cca662689fda3aff86c78f4eb5817b50
nathan <nathan.menny@etu.utc.fr>
2023-09-26 19:54:52 +0200

    Add majority vote functions

---
+++ comsoc/majority.go
@@ -0,0 +1,30 @@
+package comsoc
+
+func MajoritySWF(p Profile) (count Count, err error) {
+	if err = checkProfile(p); err != nil {
+		return
+	}
+
+	count = make(Count)
+
+	for _, alternatives := range p {
+		if _, ok := count[alternatives[0]]; !ok {
+			count[alternatives[0]] = 1
+		} else {
+			count[alternatives[0]]++
+		}
+	}
+
+	return
+}
+
+func MajoritySCF(p Profile) (bestAlts []Alternative, err error) {
+	count, err := MajoritySWF(p)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bestAlts = maxCount(count)
+	return
+}
