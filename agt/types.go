fd812248c1395daeab31cfa0a49f963abce3e530
Nathan <nathan.menny@etu.utc.fr>
2023-11-01 07:47:46 +0100

    Add comments in agt package

--- agt/types.go
+++ agt/types.go
@@ -1,25 +1,25 @@
+// Package agt contains the different agents available in the project (voters and ballot)
+
 package agt
 
 import "gitlab.utc.fr/mennynat/ia04-tp/comsoc"
 
+// The type of the Agent identifier.
 type AgentID int
 
+// A simple voting agent.
 type Agent struct {
-	ID    AgentID
-	Name  string
-	Prefs []comsoc.Alternative
+	ID    AgentID              // The ID of the agent; must be unique
+	Name  string               // The Name of the agent
+	Prefs []comsoc.Alternative // The preferences of the agent ordered from the most to the least preferred.
 }
 
+// An interface to describe the methods of a voting agent
 type AgentI interface {
-	Equal(ag AgentI) bool
-	DeepEqual(ag AgentI) bool
-	Clone() AgentI
-	String() string
-	Prefers(a comsoc.Alternative, b comsoc.Alternative) bool
-	Start()
-}
-
-type Vote struct {
-	Agt Agent
-	C   chan string
+	Equal(ag AgentI) bool                                    // <tt>true</tt> if the agents have the save ID and Name, <tt>false</tt> else
+	DeepEqual(ag AgentI) bool                                // <tt>true</tt> if the agents are equals even regarding their preferences, <tt>false</tt> else
+	Clone() AgentI                                           // Creates a duplicate of the current agent
+	String() string                                          // Gives a string representing the agent
+	Prefers(a comsoc.Alternative, b comsoc.Alternative) bool // <tt>true</tt> if the agent prefers the alternative a to b, <tt>false</tt> else
+	Start()                                                  // Starts the agent so as to it can vote
 }
